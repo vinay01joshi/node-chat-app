299151b9c617276c399f679ed396f45d85052a0c
Vinay Joshi <vinay01joshi@gmail.com>
2017-03-06 19:16:47 +0530

    Greet new uasr, and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,31 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
+    socket.emit('newMessage',{
+        from : 'Admin',
+        text : 'Welcome to the chat app.',
+        cratedAt : new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from : 'Admin',
+        text : 'New user joined.',
+        cratedAt : new Date().getTime()
+    });
+
     socket.on('createMessage',(message)=>{
-        console.log('Create Message',message);
+        console.log('Create Message',message);  
         io.emit('newMessage',{
             from : message.from,
             text : message.text,
             cratedAt : new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage',{
+        //     from : message.from,
+        //     text: message.text,
+        //     cratedAt : new Date().getTime()
+        // });
     });
 
     socket.on('disconnect',()=>{
