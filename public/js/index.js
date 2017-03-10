38830aef8b49aca5c8a5fc6554ed1ff8ebabb46c
Vinay Joshi <vinay01joshi@gmail.com>
2017-03-10 18:57:42 +0530

    Add from for messages and show incomming messages in brower

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,23 @@
 
    socket.on('disconnect', function () {
        console.log('Disconnected from server.');
-   })
+   });
 
-   socket.on('newMessage',function(message){
-       console.log('New Message',message);
-   })
+   socket.on('newMessage', function (message) {
+       console.log('New Message', message);
+
+       var li = jQuery('<li></li>');
+       li.text(`${message.from}: ${message.text}`);
+       jQuery('#messages').append(li);
+
+   });
+
+   jQuery('#message-form').on('submit', function (e) {
+       e.preventDefault();
+       socket.emit('createMessage', {
+           from: 'User',
+           text: jQuery('[name=message]').val()
+       }, function () {
+
+       });
+   });
